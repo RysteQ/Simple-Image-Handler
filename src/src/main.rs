f3a2ddc87c31f7313864a31f50b9a146e4787bdd
RysteQ <640xdm460ds4@gmail.com>
2022-09-10 22:25:59 +0300

    Added handle_error function and TODO comments

--- src/src/main.rs
+++ src/src/main.rs
@@ -18,29 +18,37 @@ fn main() {
 
     match command.process_type {
         ProcessTypeEnum::Grayscale => {
+            if check_arguments_validity(command.extra_parameters, vec![], 0) == false {
+                handle_error(-1, "Invalid extra parameters, there should be no extra arguments");
+            }
 
+            // TODO: new file.rs for grayscale convertion
         },
 
         ProcessTypeEnum::Resize => {
-
+            // TODO: Make it that it will accept any number for width and height
         },
 
         ProcessTypeEnum::ConvertToAsciiCharacters => {
-
+            // TODO: new file.rs for ASCII convertion
         },
 
         ProcessTypeEnum::SingleOutColour => {
-
+            // TODO: new file.rs to single out a certain colour in an image
         }
     }
 
-    // TODO, print out the ms it took to finish the operation
+    // TODO: print out the ms it took to finish the operation
     println!("Done !");
 }
 
-fn check_arguments_validity(arguments_vector: Vec<String>, accepted_arguments: Vec<String>, maximum_length: usize) -> bool {
-    if arguments_vector.len() > maximum_length {
+fn check_arguments_validity(arguments_vector: Vec<String>, accepted_arguments: Vec<String>, accepted_length: usize) -> bool {
+    if arguments_vector.len() != accepted_length {
         return false;
+    }
+
+    if accepted_length == 0 {
+        return true;
     }
 
     for i in 0..arguments_vector.len() {
@@ -52,4 +60,10 @@ fn check_arguments_validity(arguments_vector: Vec<String>, accepted_arguments: V
     }
 
     return false;
+}
+
+// TODO: maybe add a description parameter for how a command should be structured ?
+fn handle_error(error_code: i32, error_msg: &str) {
+    println!("{error_msg}");
+    std::process::exit(error_code);
 }
